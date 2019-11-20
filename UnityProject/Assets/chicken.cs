05dfbd583b1863cb90f4925c34ef76f1bf4d45ec
zaidaimo <55180654+zaidaimo@users.noreply.github.com>
2019-11-20 15:56:51 +0800

    G8雞參數設定

---
+++ UnityProject/Assets/chicken.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chicken : MonoBehaviour
+{
+
+    [Header("移動速度")]
+    public int speed = 10;
+    [Header("旋轉速度")]
+    public float turn = 20.5f;
+    [Header("物件名稱")]
+    public string _name = "G8";
+    public bool mission;
+
+    public void G8(float speed, int scream, int pickup, int check, string direction = "前方", string mission = "關閉")
+    {
+        print("跑步速度:" + speed);
+        print("亂叫次數:" + scream);
+        print("撿東西次數:" + pickup);
+        print("旋轉方向:" + direction);
+        print("檢視任務:" + mission);
+
+    }
+
+    public void Start()
+    {
+        G8(10.5f, 5, 2, mission: "開啟");
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
---
+++ UnityProject/Assets/learn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class learn : MonoBehaviour
+{
+   private void Drive(int speed)
+    {
+        print("開車,時速:"+ speed);
+    }
+
+    private void Shoot(int count, float speed, string prop = "無", string direction = "前方")
+    {
+        print("弓箭數量" + count);
+        print("弓箭速度" + speed);
+        print("弓箭屬性" + prop);
+        print("弓箭方向" + direction);
+
+    }
+
+    private void Start()
+    {
+        print("Hello World");
+        Drive(200);
+        Drive(300);
+
+        Shoot(1, 1.5f);
+        Shoot(10, 10.5f, direction:"前後方");
+        Shoot(10, 10.5f, prop:"火屬性", direction: "前後方");
+    }
+}
